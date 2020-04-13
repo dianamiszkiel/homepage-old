8422f86eedf03ee40a4f2dad3e39602b94016c0e
Diana <miszkiel.diana@gmail.com>
2020-04-13 19:20:03 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('siema!');
+const name = 'Maciek Kot';
+const age = 31;
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+alert("hello!");
